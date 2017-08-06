cb6a7cf33f93abe708b4447c386abe9f96ba88ce
AnthonyWaithaka <athumbi1@gmail.com>
2017-08-06 12:48:56 +0300

    FEATURE: Run.py file as entry point for the application

---
+++ run.py
@@ -0,0 +1,12 @@
+# run.py
+"""Entry point for the application
+"""
+
+import os
+from app import create_app
+
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
+
+if __name__ == '__main__':
+    app.run()
