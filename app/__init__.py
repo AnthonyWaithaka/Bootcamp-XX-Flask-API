e17fcb6d4f857bebc611affeaa5bddacc04194b4
AnthonyWaithaka <athumbi1@gmail.com>
2017-08-09 17:13:13 +0300

    FEATURE: Functions to handle bucketlists and activities

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app(config_name):
     """Create new Flask object and
     connect to database
     """
-    from app.models import User
+    from app.models import User, Bucketlist, Activity
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
@@ -25,6 +25,8 @@ def create_app(config_name):
 
     @app.route('/auth/register', methods=['POST', 'GET'])
     def register():
+        """Register a new user account
+        """
         if request.method == "POST":
             name = str(request.data.get('name', ''))
             email = str(request.data.get('email', ''))
@@ -55,4 +57,152 @@ def create_app(config_name):
             response = jsonify(results)
             response.status_code = 200
             return response
+
+    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        """Create a new bucketlist entry
+        """
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            date = str(request.data.get('date', ''))
+            description = str(request.data.get('description', ''))
+            if name:
+                bucketlist = Bucketlist(name, date, description)
+                bucketlist.save()
+                response = jsonify({
+                    'id':bucketlist.id,
+                    'name':bucketlist.bucketlist_name,
+                    'date':bucketlist.deadline_date,
+                    'description':bucketlist.bucketlist_description
+                })
+                response.status_code = 201
+                return response
+        else:
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id':bucketlist.id,
+                    'name':bucketlist.bucketlist_name,
+                    'date':bucketlist.deadline_date,
+                    'description':bucketlist.bucketlist_description
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+    @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    def bucketlist_return(id, **kwargs):
+        """Return a bucketlist by id
+        """
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        if not bucketlist:
+            abort(404)
+
+        if request.method == 'DELETE':
+            bucketlist.delete()
+            return {
+                "message":"bucketlist {} deleted successfully".format(bucketlist.id)}, 200
+        elif request.method == 'PUT':
+            name = str(request.data.get('name', ''))
+            date = str(request.data.get('date', ''))
+            description = str(request.data.get('description', ''))
+            bucketlist.bucketlist_name = name
+            bucketlist.deadline_date = date
+            bucketlist.bucketlist_description = description
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.bucketlist_name,
+                'date': bucketlist.deadline_date,
+                'description': bucketlist.bucketlist_description
+            })
+            response.status_code = 200
+            return response
+        else:
+            #GET
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.bucketlist_name,
+                'date': bucketlist.deadline_date,
+                'description': bucketlist.bucketlist_description
+            })
+            response.status_code = 200
+            return response
+
+    @app.route('/bucketlists/<int:bucketlist_id>/items', methods=['POST', 'GET'])
+    def activities(bucketlist_id, **kwargs):
+        """Create a new activity
+        """
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            description = str(request.data.get('description', ''))
+            if name:
+                activity = Activity(bucketlist_id, name, description)
+                activity.save()
+                response = jsonify({
+                    'id':activity.id,
+                    'bucketlist_id':activity.bucketlist_id,
+                    'name':activity.activity_name,
+                    'description':activity.activity_description
+                })
+                response.status_code = 201
+                return response
+        else:
+            #Return all activities
+            activities = Activity.get_all()
+            results = []
+
+            for activity in activities:
+                if activity.bucketlist_id == bucketlist_id:
+                    obj = {
+                        'id':activity.id,
+                        'bucketlist_id':activity.bucketlist_id,
+                        'name':activity.activity_name,
+                        'description':activity.activity_description
+                    }
+                    results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
+
+    @app.route('/bucketlists/<int:bucketlist_id>/items/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    def activity_manipulate(bucketlist_id, id, **kwargs):
+        """Return an activity by id for PUT and DELETE operations
+        """
+        activity = Activity.query.filter_by(id=id).first()
+        if not activity:
+            abort(404)
+
+        if request.method == 'DELETE':
+            activity.delete()
+            return {
+                "message":"activity {} deleted successfully".format(activity.id)}, 200
+        elif request.method == 'PUT':
+            name = str(request.data.get('name', ''))
+            description = str(request.data.get('description', ''))
+            activity.activity_name = name
+            activity.activity_description = description
+            activity.save()
+            response = jsonify({
+                'id': activity.id,
+                'bucketlist_id': activity.bucketlist_id,
+                'name': activity.activity_name,
+                'description': activity.activity_description
+            })
+            response.status_code = 200
+            return response
+        else:
+            #GET
+            response = jsonify({
+                'id': activity.id,
+                'bucketlist_id': activity.bucketlist_id,
+                'name': activity.activity_name,
+                'description': activity.activity_description
+            })
+            response.status_code = 200
+            return response
+
     return app
