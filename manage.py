7725c71d39690a1c7c868583ade20d35b6a1b63f
AnthonyWaithaka <athumbi1@gmail.com>
2017-08-16 16:24:37 +0300

    FEATURE: Added automated tests to manager file

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 # manage.py
 
 import os
+import unittest
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
@@ -10,7 +11,22 @@ app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
+#migration command will always be preceded by the word "db"
+#Usage: python manage.py db init*
+#Usage: replace init with migrate or upgrade
 manager.add_command('db', MigrateCommand)
+
+#command for testing
+#Usage: python manage.py test
+@manager.command
+def test():
+    """Run unit tests without test coverage
+    """
+    tests = unittest.TestLoader().discover('./test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
