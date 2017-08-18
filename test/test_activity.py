0e6683174f756d50b8b55a68219adf41c476f5b4
AnthonyWaithaka <athumbi1@gmail.com>
2017-08-18 15:21:59 +0300

    TEST: Updated activity tests to check for user token in header

--- test/test_activity.py
+++ test/test_activity.py
@@ -13,41 +13,98 @@ class ActivityTestCase(unittest.TestCase):
         """
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.bucketlist = {'name':'list1', 'date':'01012018', 'description':'Some description'}
-        self.activity = {'name':'activity1', 'description':'Do stuff'}
+        self.bucketlist = {'name':'list1', 'date':'01011999', 'description':'Some description'}
+        self.activity1 = {'name':'activity1', 'description':'Do stuff'}
+        self.activity2 = {'name':'activity2', 'description':'Do a little more stuff'}
 
         # bind the app to the current context
         with self.app.app_context():
             # create all tables
+            db.session.close()
+            db.drop_all()
             db.create_all()
+
+    def register_user(self, name="guy2", email="guy2@yes.com", password="bbb"):
+        """Register a new user for the activity tests
+        """
+        user_data = {
+            'name': name,
+            'email': email,
+            'password': password
+        }
+        return self.client().post('/auth/register', data=user_data)
+
+    def login_user(self, email="guy2@yes.com", password="bbb"):
+        """Log in with the registered account
+        """
+        user_data = {
+            'email': email,
+            'password': password
+        }
+        return self.client().post('/auth/login', data=user_data)
+
+    def create_bucketlist(self, access_token):
+        """Create a new bucketlist to test activities
+        """
+        result = self.client().post('/bucketlists/',
+                                    headers=dict(Authorization="Bearer " + access_token),
+                                    data=self.bucketlist)
+        self.assertEqual(result.status_code, 201, msg="Page not returned")
+        self.assertIn('list1', str(result.data), msg="Bucketlist not created")
+
 
     def test_activity_creation(self):
         """Test API can create an activity (POST)
         """
-        rb = self.client().post('/bucketlists/', data=self.bucketlist)
-        result = self.client().post('/bucketlists/1/items', data=self.activity)
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+        self.create_bucketlist(access_token)
+        result = self.client().post('/bucketlists/1/items',
+                                headers=dict(Authorization="Bearer " + access_token),
+                                data=self.activity1)
         self.assertEqual(result.status_code, 201)
         self.assertIn('activity1', str(result.data))
 
     def test_api_return_all_activities(self):
         """Test that the API can return all activities (GET)
         """
-        rb = self.client().post('/bucketlists/', data=self.bucketlist)
-        result = self.client().post('/bucketlists/1/items', data=self.activity)
-        self.assertEqual(result.status_code, 201)
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+        self.create_bucketlist(access_token)
+        result1 = self.client().post('/bucketlists/1/items',
+                                headers=dict(Authorization="Bearer " + access_token),
+                                data=self.activity1)
+        result2 = self.client().post('/bucketlists/1/items',
+                                headers=dict(Authorization="Bearer " + access_token),
+                                data=self.activity2)
+        self.assertEqual(result1.status_code, 201)
+        self.assertEqual(result2.status_code, 201)
         result = self.client().get('/bucketlists/1/items')
         self.assertEqual(result.status_code, 200)
-        self.assertIn('Do stuff', str(result.data))
+        self.assertIn('Do stuff', str(result2.data))
+        self.assertIn('Do a little more stuff', str(result2.data))
 
     def test_activity_editing(self):
         """Test API can edit an activity (PUT)
         """
-        rb = self.client().post('/bucketlists/', data=self.bucketlist)
-        result = self.client().post('/bucketlists/1/items', data=self.activity)
-        self.assertEqual(result.status_code, 201)
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+        self.create_bucketlist(access_token)
+        result1 = self.client().post('/bucketlists/1/items',
+                                headers=dict(Authorization="Bearer " + access_token),
+                                data=self.activity1)
+        result2 = self.client().post('/bucketlists/1/items',
+                                headers=dict(Authorization="Bearer " + access_token),
+                                data=self.activity1)
+        self.assertEqual(result1.status_code, 201)
+        self.assertEqual(result2.status_code, 201)
         result = self.client().put(
             '/bucketlists/1/items/1',
-            data={'bucketlist_id':1, 'name':'activity2', 'description':'New thing'})
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'bucketlist_id':1, 'name':'activitynew', 'description':'New thing'})
         self.assertEqual(result.status_code, 200)
         new_result = self.client().get('/bucketlists/1/items')
         self.assertIn('New thing', str(new_result.data))
@@ -55,9 +112,18 @@ class ActivityTestCase(unittest.TestCase):
     def test_activity_deleting(self):
         """Test API can delete an activity (DELETE)
         """
-        rb = self.client().post('/bucketlists/', data=self.bucketlist)
-        result = self.client().post('/bucketlists/1/items', data=self.activity)
-        self.assertEqual(result.status_code, 201)
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+        self.create_bucketlist(access_token)
+        result1 = self.client().post('/bucketlists/1/items',
+                                headers=dict(Authorization="Bearer " + access_token),
+                                data=self.activity1)
+        result2 = self.client().post('/bucketlists/1/items',
+                                headers=dict(Authorization="Bearer " + access_token),
+                                data=self.activity1)
+        self.assertEqual(result1.status_code, 201)
+        self.assertEqual(result2.status_code, 201)
         delete_result = self.client().delete('/bucketlists/1/items/1')
         self.assertEqual(delete_result.status_code, 200)
         #Test for data, should return a 404
