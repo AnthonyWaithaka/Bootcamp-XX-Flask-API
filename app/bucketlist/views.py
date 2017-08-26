c23c042b76ec71c35572b4c2bcb7a65fb561e1d1
AnthonyGW <anthony.waithaka@andela.com>
2017-08-26 20:33:02 +0300

    UPDATE: Use decorator to authenticate users in all request handling methods. Minor code clean up and additional authentication.

--- app/bucketlist/views.py
+++ app/bucketlist/views.py
@@ -10,15 +10,10 @@ bucketlist_blueprint = Blueprint('bucketlist', __name__)
 from flask.views import MethodView
 from flask import make_response, request, jsonify, abort
 from app.models import Bucketlist, User, Blacklist
+from app.app import user_id
 
-class BucketListsView(MethodView):
-    """Request handling for the bucketlists
-    that belong to the user currently logged in
-    """
-    def get(self):
-        """GET request handling for /bucketlists/
-        Return all the user's bucketlists
-        """
+def authentication_required(request_method):
+    def wrapper(*args, **kwargs):
         auth_header = request.headers.get('Authorization')
         access_token = auth_header.split(" ")[1]
 
@@ -27,162 +22,127 @@ class BucketListsView(MethodView):
             if blacklisted:
                 response = {'message':'Please log in to access bucketlists.'}
                 return make_response(jsonify(response)), 401
-
+            global user_id
             user_id = User.decode_token(access_token)
             if not isinstance(user_id, str):
                 # user has been authenticated
-                bucketlists = Bucketlist.query.filter_by(user_id=user_id)
-                results = []
-
-                for bucketlist in bucketlists:
-                    obj = {
-                        'user':bucketlist.user_id,
-                        'id':bucketlist.id,
-                        'name':bucketlist.bucketlist_name,
-                        'date':bucketlist.deadline_date,
-                        'description':bucketlist.bucketlist_description
-                    }
-                    results.append(obj)
-
-                if results == []:
-                    return make_response({'message':'No bucketlists to view'}), 204
-
-                response = jsonify(results)
-                response.status_code = 200
-                return response
+                return request_method(*args, **kwargs)
             else:
                 message = user_id
                 response = {'message': message}
                 return make_response(jsonify(response)), 400
+        else:
+            return make_response(jsonify({'message':'Unauthorized attempt.'})), 401
+    return wrapper
 
+class BucketListsView(MethodView):
+    """Request handling for the bucketlists
+    that belong to the user currently logged in
+    """
+    @authentication_required
+    def get(self):
+        """GET request handling for /bucketlists/
+        Return all the user's bucketlists
+        """
+        bucketlists = Bucketlist.query.filter_by(user_id=user_id)
+        results = []
+
+        for bucketlist in bucketlists:
+            obj = {
+                'user':bucketlist.user_id,
+                'id':bucketlist.id,
+                'name':bucketlist.bucketlist_name,
+                'date':bucketlist.deadline_date,
+                'description':bucketlist.bucketlist_description
+            }
+            results.append(obj)
+
+        if results == []:
+            return make_response({'message':'No bucketlists to view'}), 204
+
+        response = jsonify(results)
+        response.status_code = 200
+        return response
+
+    @authentication_required
     def post(self):
         """POST request handling for /bucketlists/
         Create a new bucketlist for the user
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlists.'}
-            return make_response(jsonify(response)), 401
-
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                name = str(request.data.get('name', ''))
-                date = str(request.data.get('date', ''))
-                description = str(request.data.get('description', ''))
-                if name:
-                    bucketlist = Bucketlist(user_id, name, date, description)
-                    bucketlist.save()
-                    response = jsonify({
-                        'user':bucketlist.user_id,
-                        'id':bucketlist.id,
-                        'name':bucketlist.bucketlist_name,
-                        'date':bucketlist.deadline_date,
-                        'description':bucketlist.bucketlist_description
-                    })
-                    response.status_code = 201
-                    return response
+        name = str(request.data.get('name', ''))
+        date = str(request.data.get('date', ''))
+        description = str(request.data.get('description', ''))
+        if name:
+            bucketlist = Bucketlist(user_id, name, date, description)
+            bucketlist.save()
+            response = jsonify({
+                'user':bucketlist.user_id,
+                'id':bucketlist.id,
+                'name':bucketlist.bucketlist_name,
+                'date':bucketlist.deadline_date,
+                'description':bucketlist.bucketlist_description
+            })
+            response.status_code = 201
+            return response
 
 class BucketListsManipulationView(MethodView):
     """Request handling for manipulating the bucketlists
     that belong to the user
     """
+    @authentication_required
     def delete(self, list_id):
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlists.'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                bucketlist = Bucketlist.query.filter_by(id=list_id).first()
-                if not bucketlist:
-                    abort(404)
-                else:
-                    bucketlist.delete()
-                    return {
-                        "message":"bucketlist {} deleted successfully".format(bucketlist.id)}, 200
-            else:
-                message = user_id
-                response = {'message':message}
-                return make_response(jsonify(response)), 400
+        bucketlist = Bucketlist.query.filter_by(id=list_id).first()
+        if not bucketlist:
+            abort(404)
+        else:
+            bucketlist.delete()
+            return {
+                "message":"bucketlist {} deleted successfully".format(bucketlist.id)}, 200
 
+    @authentication_required
     def put(self, list_id):
         """PUT request handling for /bucketlists/<int:list_id>
         Update a bucketlist's details
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlists.'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                bucketlist = Bucketlist.query.filter_by(id=list_id).first()
-                if not bucketlist:
-                    abort(404)
-                else:
-                    name = str(request.data.get('name', ''))
-                    date = str(request.data.get('date', ''))
-                    description = str(request.data.get('description', ''))
-                    bucketlist.bucketlist_name = name
-                    bucketlist.deadline_date = date
-                    bucketlist.bucketlist_description = description
-                    bucketlist.save()
-                    response = jsonify({
-                        'id': bucketlist.id,
-                        'name': bucketlist.bucketlist_name,
-                        'date': bucketlist.deadline_date,
-                        'description': bucketlist.bucketlist_description,
-                        'user_id': bucketlist.user_id
-                    })
-                    response.status_code = 200
-                    return response
-            else:
-                message = user_id
-                response = {'message':message}
-                return make_response(jsonify(response)), 400
+        bucketlist = Bucketlist.query.filter_by(id=list_id).first()
+        if not bucketlist:
+            abort(404)
+        else:
+            name = str(request.data.get('name', ''))
+            date = str(request.data.get('date', ''))
+            description = str(request.data.get('description', ''))
+            bucketlist.bucketlist_name = name
+            bucketlist.deadline_date = date
+            bucketlist.bucketlist_description = description
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.bucketlist_name,
+                'date': bucketlist.deadline_date,
+                'description': bucketlist.bucketlist_description,
+                'user_id': bucketlist.user_id
+            })
+            response.status_code = 200
+            return response
 
+    @authentication_required
     def get(self, list_id):
         """GET request handling for /bucketlists/<int:list_id>
         Return the user's bucketlist data
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlists.'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                bucketlist = Bucketlist.query.filter_by(id=list_id).first()
-                if not bucketlist:
-                    abort(404)
-                else:
-                    response = jsonify({
-                        'id': bucketlist.id,
-                        'user_id': bucketlist.user_id,
-                        'name': bucketlist.bucketlist_name,
-                        'date': bucketlist.deadline_date,
-                        'description': bucketlist.bucketlist_description
-                    })
-                    response.status_code = 200
-                    return response
-            else:
-                message = user_id
-                response = {'message':message}
-                return make_response(jsonify(response)), 400
+        bucketlist = Bucketlist.query.filter_by(id=list_id).first()
+        if not bucketlist:
+            abort(404)
+        else:
+            response = jsonify({
+                'id': bucketlist.id,
+                'user_id': bucketlist.user_id,
+                'name': bucketlist.bucketlist_name,
+                'date': bucketlist.deadline_date,
+                'description': bucketlist.bucketlist_description
+            })
+            response.status_code = 200
+            return response
 
 bucketlists_view = BucketListsView.as_view('bucketlists_view')
 bucketlistsmanip_view = BucketListsManipulationView.as_view('bucketlistsmanip_view')
--- app/bucketlist_item/views.py
+++ app/bucketlist_item/views.py
@@ -10,170 +10,132 @@ bucketlist_item_blueprint = Blueprint('bucketlist_item', __name__)
 from flask.views import MethodView
 from flask import make_response, request, jsonify, abort
 from app.models import Bucketlist, User, BucketlistItem, Blacklist
+from app.app import user_id
+
+def authentication_required(request_method):
+    def wrapper(*args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
+
+        if access_token:
+            blacklisted = Blacklist.query.filter_by(used_token=access_token).first()
+            if blacklisted:
+                response = {'message':'Please log in to access bucketlists.'}
+                return make_response(jsonify(response)), 401
+            global user_id
+            user_id = User.decode_token(access_token)
+            if not isinstance(user_id, str):
+                bucketlist = Bucketlist.query.filter_by(id=kwargs['list_id']).first()
+                if bucketlist.user_id != user_id:
+                    return make_response(jsonify({'message':'Unauthorized access to this bucketlist.'}))
+                # user has been authenticated
+                return request_method(*args, **kwargs)
+            else:
+                message = user_id
+                response = {'message': message}
+                return make_response(jsonify(response)), 400
+        else:
+            return make_response(jsonify({'message':'Unauthorized attempt.'})), 401
+    return wrapper
 
 class ActivitiesView(MethodView):
     """Request handling for bucketlist items
     """
+    @authentication_required
     def get(self, list_id):
         """GET request handling for /bucketlists/<int:list_id>/items
         Return all the bucketlist's items
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlist items.'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                activities = BucketlistItem.query.filter_by(bucketlist_id=list_id)
-                results = []
+        activities = BucketlistItem.query.filter_by(bucketlist_id=list_id)
+        results = []
 
-                for bucketlist_item in activities:
-                    obj = {
-                        'id':bucketlist_item.id,
-                        'bucketlist_id':bucketlist_item.bucketlist_id,
-                        'name':bucketlist_item.bucketlist_item_name,
-                        'description':bucketlist_item.bucketlist_item_description
-                    }
-                    results.append(obj)
-                response = jsonify(results)
-                response.status_code = 200
-                return response
-            else:
-                message = user_id
-                response = {'message': message}
-                return make_response(jsonify(response)), 400
+        for bucketlist_item in activities:
+            obj = {
+                'id':bucketlist_item.id,
+                'bucketlist_id':bucketlist_item.bucketlist_id,
+                'name':bucketlist_item.bucketlist_item_name,
+                'description':bucketlist_item.bucketlist_item_description
+            }
+            results.append(obj)
+        response = jsonify(results)
+        response.status_code = 200
+        return response
 
+    @authentication_required
     def post(self, list_id):
         """POST request handling for /bucketlists/<int:list_id>/items
         Create a new bucketlist item
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlist items.'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                name = str(request.data.get('name', ''))
-                description = str(request.data.get('description', ''))
-                if name:
-                    bucketlist_item = BucketlistItem(list_id, name, description)
-                    bucketlist_item.save()
-                    response = jsonify({
-                        'id':bucketlist_item.id,
-                        'bucketlist_id':bucketlist_item.bucketlist_id,
-                        'name':bucketlist_item.bucketlist_item_name,
-                        'description':bucketlist_item.bucketlist_item_description
-                    })
-                    response.status_code = 201
-                    return response
-            else:
-                message = user_id
-                response = {'message': message}
-                return make_response(jsonify(response)), 400
+        name = str(request.data.get('name', ''))
+        description = str(request.data.get('description', ''))
+        if name:
+            bucketlist_item = BucketlistItem(list_id, name, description)
+            bucketlist_item.save()
+            response = jsonify({
+                'id':bucketlist_item.id,
+                'bucketlist_id':bucketlist_item.bucketlist_id,
+                'name':bucketlist_item.bucketlist_item_name,
+                'description':bucketlist_item.bucketlist_item_description
+            })
+            response.status_code = 201
+            return response
 
 class ActivitiesManipulationView(MethodView):
     """Request handling for manipulating bucketlist items
     """
+    @authentication_required
     def delete(self, list_id, item_id):
-        """DELETE request handling for /bucketlists/<int:list_id>/items/<int:item_id>
+        """_DELETE request handling for /bucketlists/<int:list_id>/items/<int:item_id>
         Delete the bucketlist item
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlist items'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                bucketlist_item = BucketlistItem.query.filter_by(id=item_id).first()
-                if not bucketlist_item:
-                    abort(404)
-                else:
-                    bucketlist_item.delete()
-                    return {
-                        "message":"bucketlist_item {} deleted successfully".format(bucketlist_item.id)}, 200
-            else:
-                message = user_id
-                response = {'message':message}
-                return make_response(jsonify(response)), 400
+        bucketlist_item = BucketlistItem.query.filter_by(id=item_id).first()
+        if not bucketlist_item:
+            abort(404)
+        else:
+            bucketlist_item.delete()
+            return {
+                "message":"bucketlist_item {} deleted successfully".format(bucketlist_item.id)}, 200
 
+    @authentication_required
     def put(self, list_id, item_id):
         """PUT request handling for /bucketlists/<int:list_id>/items/<int:item_id>
         Update a bucketlist item's details
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlist items.'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                bucketlist_item = BucketlistItem.query.filter_by(id=item_id).first()
-                if not bucketlist_item:
-                    abort(404)
-                else:
-                    name = str(request.data.get('name', ''))
-                    description = str(request.data.get('description', ''))
-                    bucketlist_item.bucketlist_item_name = name
-                    bucketlist_item.bucketlist_item_description = description
-                    bucketlist_item.save()
-                    response = jsonify({
-                        'id': bucketlist_item.id,
-                        'bucketlist_id': bucketlist_item.bucketlist_id,
-                        'name': bucketlist_item.bucketlist_item_name,
-                        'description': bucketlist_item.bucketlist_item_description
-                    })
-                    response.status_code = 200
-                    return response
-            else:
-                message = user_id
-                response = {'message':message}
-                return make_response(jsonify(response)), 400
+        bucketlist_item = BucketlistItem.query.filter_by(id=item_id).first()
+        if not bucketlist_item:
+            abort(404)
+        else:
+            name = str(request.data.get('name', ''))
+            description = str(request.data.get('description', ''))
+            bucketlist_item.bucketlist_item_name = name
+            bucketlist_item.bucketlist_item_description = description
+            bucketlist_item.save()
+            response = jsonify({
+                'id': bucketlist_item.id,
+                'bucketlist_id': bucketlist_item.bucketlist_id,
+                'name': bucketlist_item.bucketlist_item_name,
+                'description': bucketlist_item.bucketlist_item_description
+            })
+            response.status_code = 200
+            return response
 
+    @authentication_required
     def get(self, list_id, item_id):
         """GET request handling for /bucketlists/<int:list_id>/items/<int:item_id>
         Return the bucketlist item's data
         """
-        auth_header = request.headers.get('Authorization')
-        access_token = auth_header.split(" ")[1]
-        blacklist = Blacklist.query.filter_by(used_token=access_token).first()
-        if blacklist:
-            response = {'message':'Please log in to access bucketlist items.'}
-            return make_response(jsonify(response)), 401
-        if access_token:
-            user_id = User.decode_token(access_token)
-            if not isinstance(user_id, str):
-                # user has been authenticated
-                bucketlist_item = BucketlistItem.query.filter_by(id=item_id).first()
-                if not bucketlist_item:
-                    abort(404)
-                else:
-                    response = jsonify({
-                        'id': bucketlist_item.id,
-                        'bucketlist_id': bucketlist_item.bucketlist_id,
-                        'name': bucketlist_item.bucketlist_item_name,
-                        'description': bucketlist_item.bucketlist_item_description
-                    })
-                    response.status_code = 200
-                    return response
-            else:
-                message = user_id
-                response = {'message':message}
-                return make_response(jsonify(response)), 400
+        bucketlist_item = BucketlistItem.query.filter_by(id=item_id).first()
+        if not bucketlist_item:
+            abort(404)
+        else:
+            response = jsonify({
+                'id': bucketlist_item.id,
+                'bucketlist_id': bucketlist_item.bucketlist_id,
+                'name': bucketlist_item.bucketlist_item_name,
+                'description': bucketlist_item.bucketlist_item_description
+            })
+            response.status_code = 200
+            return response
 
 activities_view = ActivitiesView.as_view('activities_view')
 activitiesmanip_view = ActivitiesManipulationView.as_view('activitiesmanip_view')
